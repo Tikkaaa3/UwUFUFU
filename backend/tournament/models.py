458a331f544fc4e7a26155d92c9a532e1ddda02d
ahmetysfd <ahmetysfd2002@gmail.com>
2025-02-06 13:57:28 +0100

    Update models.py

--- backend/tournament/models.py
+++ backend/tournament/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Character(models.Model):
     name = models.CharField(max_length=100)
-    img = models.URLField()  # Store the image URL
+    img = models.ImageField(upload_to='characters/')  # Store images in media/characters/
+    # img = models.URLField()  # Store the image URL
 
     def __str__(self):
         return self.name
