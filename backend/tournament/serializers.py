4b894accc6e923b8116aec49b2259ffa3aeb268c
patron2323 <yusufkaratasogly219@gmail.com>
2025-02-06 14:06:49 +0100

    Update serializers.py

--- backend/tournament/serializers.py
+++ backend/tournament/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Character
 
+
 class CharacterSerializer(serializers.ModelSerializer):
+    img = serializers.SerializerMethodField()
+
     class Meta:
         model = Character
         fields = ['id', 'name', 'img']
+
+    def get_img(self, obj):
+        request = self.context.get('request')
+        if obj.img and hasattr(obj.img, 'url'):
+            return request.build_absolute_uri(obj.img.url)
+        return None
